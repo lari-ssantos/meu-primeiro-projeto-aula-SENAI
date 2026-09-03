bb7c4b0abc386c1f70c26a26a33b26a2b56e5c8d
lari-ssantos <santos.souza.larissa7@gmail.com>
2026-09-03 15:22:40 -0300

    2.2 operadores lógicos

---
+++ untitled/src/main/java/listaexercicios/Main6.java
@@ -0,0 +1,14 @@
+package listaexercicios;
+class Main6 {
+    public static void main(String[] args) {
+
+        boolean nota, freq, matar;
+        nota= true;
+        freq = true;
+        matar = true;
+
+        boolean resultado = nota && freq && !matar;
+        System.out.println("Resultado " + resultado);
+
+    }
+}
