7f7c558f8054b1dae9706183a4c482b10521f773
Larissa <santos.souza.larissa7@gmail.com>
2026-09-03 14:49:50 -0300

    1.2 casting explícito
    Updated package name and corrected variable case in print statements.

---
+++ untitled/src/main/java/listaexercicios/Main2.java
@@ -0,0 +1,11 @@
+package listaexercicios;
+class Main2 {
+    public static void main(String[] args) {
+
+        double varDouble = 56.45;
+        int varInt = (int) varDouble;
+
+        System.out.println("valor double: " + varDouble);
+        System.out.println("valor int: " + varInt);
+    }
+}
