c4e675520e61668b46fc8a0d4ed11e286df802e5
lari-ssantos <santos.souza.larissa7@gmail.com>
2026-09-03 14:54:50 -0300

    1.3 conversão de tipos

---
+++ untitled/src/main/java/listaexercicios/Main3.java
@@ -0,0 +1,16 @@
+package listaexercicios;
+
+class Main3 {
+    public static void main(String[] args) {
+
+        float varFloat = 250.10f;
+        long varLong = (long) varFloat;
+        double varDouble = varLong;
+
+        System.out.println("var F: " + varFloat);
+        System.out.println("var L: " + varLong);
+        System.out.println("var D: " + varDouble);
+    }
+}
+
+
