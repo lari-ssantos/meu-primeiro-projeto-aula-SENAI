2d41851e6a152b4c266b09374d57dabe6981f307
lari-ssantos <santos.souza.larissa7@gmail.com>
2026-09-03 15:20:21 -0300

    2.1 comparação de dois números

---
+++ untitled/src/main/java/listaexercicios/Main5.java
@@ -0,0 +1,19 @@
+package listaexercicios;
+class Main5 {
+    public static void main(String[] args) {
+
+        int A, B;
+        A = 10;
+        B = 5;
+
+        boolean maior = A > B;
+
+        System.out.println("A MAIOR QUE B? " + maior);
+        System.out.println("A MENOR QUE B? " + (A < B));
+        System.out.println("A MAIOR OU IGUAL A B? " + (A >= B));
+        System.out.println("A MENOR OU IGUAL QUE B? " + (A <= B));
+        System.out.println("A IGUAL B? " + (A = B));
+        System.out.println("A DIFERENTE DE B? " + (A != B));
+
+    }
+}
