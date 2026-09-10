00ac7ed689483069af698da3e13082435711e18f
lari-ssantos <santos.souza.larissa7@gmail.com>
2026-09-10 15:31:20 -0300

    plano de saude

--- untitled/src/main/java/listaexercicios/atv7.java
+++ untitled/src/main/java/listaexercicios/atv7.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class atv7 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int tempo;
         System.out.println("digite o seu tempo na empresa ");
