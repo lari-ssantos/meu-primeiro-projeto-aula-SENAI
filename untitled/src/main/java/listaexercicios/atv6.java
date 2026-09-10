2d49edc9d185dec49c9664b7e7c362d11097b765
lari-ssantos <santos.souza.larissa7@gmail.com>
2026-09-10 15:31:02 -0300

    auxilio vr

--- untitled/src/main/java/listaexercicios/atv6.java
+++ untitled/src/main/java/listaexercicios/atv6.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class atv6 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         double salario;
         System.out.println("digite o valor do seu salário ");
