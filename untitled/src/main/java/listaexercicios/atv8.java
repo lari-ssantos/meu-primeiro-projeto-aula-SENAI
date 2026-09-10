e3eb68966b0092281dfb154cc33662be5141191c
lari-ssantos <santos.souza.larissa7@gmail.com>
2026-09-10 15:31:45 -0300

    auxilio combustivel

--- untitled/src/main/java/listaexercicios/atv8.java
+++ untitled/src/main/java/listaexercicios/atv8.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class atv8 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         String resposta;
         System.out.println("O funcionário utiliza veículo próprio? (S/N)");
